426801bf96badeaca5b919cbaf1c5678dc6f0998
AdriEspejo <adrieleespejo@gmail.com>
2025-09-25 22:18:34 -0300

    Actualización de Medico
    Implementación con lombok

--- src/main/java/org/jcr/Entidades/Medico.java
+++ src/main/java/org/jcr/Entidades/Medico.java
@@ -1,37 +1,35 @@
 package org.jcr.Entidades;
+
+import lombok.*;
+import lombok.experimental.SuperBuilder;
 import org.jcr.Entidades.Enums.EspecialidadMedica;
 import org.jcr.Entidades.Enums.TipoSangre;
 
 import java.io.Serializable;
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
+@Getter
+@ToString(callSuper = true, exclude = {"departamento", "citas"})
+@EqualsAndHashCode(callSuper = true, exclude = {"departamento", "citas"})
+@SuperBuilder
 public class Medico extends Persona implements Serializable {
+
     private final Matricula matricula;
     private final EspecialidadMedica especialidad;
+
+    @Setter
     private Departamento departamento;
+
+    @Builder.Default
     private final List<Cita> citas = new ArrayList<>();
 
+    @Builder
     public Medico(String nombre, String apellido, String dni, LocalDate fechaNacimiento,
                   TipoSangre tipoSangre, String numeroMatricula, EspecialidadMedica especialidad) {
         super(nombre, apellido, dni, fechaNacimiento, tipoSangre);
         this.matricula = new Matricula(numeroMatricula);
         this.especialidad = Objects.requireNonNull(especialidad, "La especialidad no puede ser nula");
-    }
-
-    public Matricula getMatricula() {
-        return matricula;
-    }
-
-    public EspecialidadMedica getEspecialidad() {
-        return especialidad;
-    }
-
-    public Departamento getDepartamento() {
-        return departamento;
     }
 
     public void setDepartamento(Departamento departamento) {
@@ -46,15 +44,5 @@ public class Medico extends Persona implements Serializable {
 
     public List<Cita> getCitas() {
         return Collections.unmodifiableList(new ArrayList<>(citas));
-    }
-
-    @Override
-    public String toString() {
-        return "Medico{" +
-                "nombre='" + nombre + '\'' +
-                ", apellido='" + apellido + '\'' +
-                ", especialidad=" + especialidad.getDescripcion() +
-                ", matricula=" + matricula.getNumero() +
-                '}';
     }
 }
