7e2639c6837a4e174ac04c4df880536d525f29b0
AdriEspejo <adrieleespejo@gmail.com>
2025-09-27 16:45:31 -0300

    Arreglo

--- src/main/java/org/jcr/Entidades/Sala.java
+++ src/main/java/org/jcr/Entidades/Sala.java
@@ -7,29 +7,29 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 
-
 @Getter
 @ToString(exclude = {"citas", "departamento"}) // evitamos recursividad
 @EqualsAndHashCode(of = {"numero", "departamento"}) // clave natural
-@Builder
-
 public class Sala implements Serializable {
 
     private final String numero;
     private final String tipo;
     private final Departamento departamento;
 
-    @Builder.Default
-    private final List<Cita> citas = new ArrayList<>();
+    private final List<Cita> citas;
 
+    // Constructor manual
     public Sala(String numero, String tipo, Departamento departamento) {
         this.numero = validarString(numero, "El número de sala no puede ser nulo ni vacío");
         this.tipo = validarString(tipo, "El tipo de sala no puede ser nulo ni vacío");
         this.departamento = Objects.requireNonNull(departamento, "El departamento no puede ser nulo");
+        this.citas = new ArrayList<>(); // inicializamos la lista
     }
 
     public void addCita(Cita cita) {
-        this.citas.add(cita);
+        if (cita != null && !citas.contains(cita)) {
+            this.citas.add(cita);
+        }
     }
 
     public List<Cita> getCitas() {
